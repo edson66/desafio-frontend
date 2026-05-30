93e7ff4ee5f8bb90837071f273ab18dc9e59501d
edson66 <edsonulissesr1@gmail.com>
2026-05-30 01:28:47 -0300

    feat: configura roteamento e estrutura as paginas de listagem e formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import type { Product } from './types/Product';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { ProductList } from './pages/ProductList';
+import { ProductForm } from './pages/ProductForm';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-
-  useEffect(() => {
-    axios.get('http://localhost:3001/products')
-      .then(response => {
-        console.log("Deu certo! Os dados chegaram:", response.data);
-        setProducts(response.data);
-      })
-      .catch(error => {
-        console.error("Erro na requisição:", error);
-      });
-  }, []);
-
+  
   return (
-    <div>
-      <h1>Meus Produtos</h1>
-      <ul>
-        {products.map(product => (
-          <li key={product.id}>
-            {product.name} - R$ {product.price} ({product.status})
-          </li>
-        ))}
-      </ul>
-    </div>
+    <BrowserRouter>
+      <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
+        <Link to="/" style={{ marginRight: '15px' }}>Ver Produtos</Link>
+        <Link to="/cadastro">Novo Produto</Link>
+      </nav>
+
+      <div style={{ padding: '20px' }}>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cadastro" element={<ProductForm />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ProductForm.tsx
@@ -0,0 +1,8 @@
+export function ProductForm() {
+  return (
+    <div>
+      <h1>Cadastrar Produto</h1>
+      <p>Formulário</p>
+    </div>
+  );
+}
